2fb5d95179a1b7a14f10cf03d397f3cd394a126e
VirusVIP <viruzvip@gmail.com>
2020-11-26 13:00:17 +0400

    add practice_16 and practice 18

---
+++ practice_16/rectangle.py
@@ -0,0 +1,12 @@
+class Rectangle:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+    def getWidth(self):
+        return self.width
+    def getHeight(self):
+        return self.height
+    def getArea(self):
+        return self.width * self.height
+    def getPerimeter(self):
+        return self.width*2+self.height*2
---
+++ practice_16/testRect.py
@@ -0,0 +1,12 @@
+from rectangle import Rectangle
+
+r1 = Rectangle(10,5)
+print("r1 width=",r1.width)
+print("r1 area =", r1.getArea())
+print("r1 Perimeter =", r1.getPerimeter())
+print(5*"\n")
+r2 = Rectangle(1920, 1080)
+print("r2 width=",r2.width)
+print("r2 height=",r2.height)
+print("r2 area =", r2.getArea())
+print("r2 Perimeter =", r2.getPerimeter())
