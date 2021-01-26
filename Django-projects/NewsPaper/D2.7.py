35d1e3621d0b141fb9e0544a35ec943992acb9c8
VirusVIP <viruzvip@gmail.com>
2021-01-27 02:12:16 +0400

    Remodelling, set defs

--- Django-projects/NewsPaper/D2.7.py
+++ Django-projects/NewsPaper/D2.7.py
@@ -3,55 +3,44 @@ from django.contrib.auth.models import User
 
 
 class Author(models.Model):
-    author_name = models.CharField(max_length=255, null=False)
-    rating = models.IntegerField(default=0)
-    username = models.OneToOneField(User, on_delete=models.CASCADE)
-
+    name = models.CharField(max_length = 255)
+    rating = models.IntegerField()
+    user_id = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
     def update_rating(self):
         pass
 
 
 class Category(models.Model):
-    cat_name = models.CharField(max_length=128, unique=True)
+    name = models.CharField(max_length = 128, unique=True)
 
 
 class Post(models.Model):
-    news = 'NE'
-    articles = 'AR'
-    CHOICE = [(news, 'Новость'), (articles, 'Статья')]
-
-    post_is_news = models.CharField(max_length=1, choices=CHOICE, default=news)
+    post_is_news = models.BooleanField(default=True)
     post_create_date = models.DateTimeField(null=False)
     post_header = models.CharField(max_length=255)
     post_text = models.TextField()
     post_rating = models.IntegerField(default=0)
     post_author = models.ForeignKey(Author, on_delete=models.CASCADE)
     post_category = models.ManyToManyField(Category, through='PostCategory')
-
     def like(self):
         self.post_rating += 1
-
     def dislike(self):
         self.post_rating -= 1
-
     def preview(self):
-        return self.post_text[0:100]
-
+        pass
 
 class PostCategory(models.Model):
-    post = models.ForeignKey(Post, on_delete=models.CASCADE)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    post = models.ManyToManyField(Post, on_delete=models.SET_NULL)
+    category = models.ManyToManyField(Category)
 
 
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    text = models.TextField(null=True)
+    text = models.TextField(max_length=2048)
     create_time = models.DateTimeField(null=False)
     rating = models.IntegerField(default=0)
-
     def like(self):
         self.rating += 1
-
     def dislike(self):
         self.rating -= 1
