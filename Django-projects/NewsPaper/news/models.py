defa6a548f11d89e98fcfca13e611003357296c0
VirusVIP <viruzvip@gmail.com>
2021-01-28 02:28:50 +0400

    Models functions, sqlite DB repair, commands file create

--- Django-projects/NewsPaper/news/models.py
+++ Django-projects/NewsPaper/news/models.py
@@ -3,16 +3,44 @@ from django.contrib.auth.models import User
 
 
 class Author(models.Model):
-    author_name = models.CharField(max_length=255, null=False)
+    name = models.CharField(max_length=255, null=False)
     rating = models.IntegerField(default=0)
     username = models.OneToOneField(User, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return self.name
+        # return f'Author:{self.name}, rating:{self.rating}'
+
     def update_rating(self):
-        pass
+        posts_rate = 0
+        com_rate = 0
+        cm_ps_rate = 0
+        for ps in Post.objects.filter(author=self.pk):
+            # print(f"Post({ps.pk}) rating:{ps.rating}")
+            posts_rate += ps.rating*3
+            for cm_ps in Comment.objects.filter(post=ps.pk):
+                # print(f"Post({ps.pk}) comment({cm_ps.pk}) rating: {cm_ps.rating}")
+                cm_ps_rate += cm_ps.rating
+        for cm in Comment.objects.filter(user=self.pk):
+            # print(f"Comment({cm.pk}) rating:{cm.rating}")
+            com_rate += cm.rating
+        overall_rating = posts_rate+com_rate+cm_ps_rate
+
+        # print(f'----------\nPosts rating:{posts_rate}')
+        # print(f'Comments rating:{com_rate}')
+        # print(f'All Posts - Comments rating:{cm_ps_rate}')
+        # print(f'----------\nOVERALL rating:{overall_rating}')
+        self.rating = overall_rating
+        self.save()
+
 
 
 class Category(models.Model):
-    cat_name = models.CharField(max_length=128, unique=True)
+    name = models.CharField(max_length=128, unique=True)
+
+    def __str__(self):
+        # return f'[ID:{self.pk}], name:{self.name}'
+        return self.name
 
 
 class Post(models.Model):
@@ -20,38 +48,54 @@ class Post(models.Model):
     articles = 'AR'
     CHOICE = [(news, 'Новость'), (articles, 'Статья')]
 
-    post_is_news = models.CharField(max_length=1, choices=CHOICE, default=news)
-    post_create_date = models.DateTimeField(null=False)
-    post_header = models.CharField(max_length=255)
-    post_text = models.TextField()
-    post_rating = models.IntegerField(default=0)
-    post_author = models.ForeignKey(Author, on_delete=models.CASCADE)
-    post_category = models.ManyToManyField(Category, through='PostCategory')
+    type = models.CharField(max_length=2, choices=CHOICE, default='NE')
+    create_date = models.DateTimeField(auto_now_add=True)
+    header = models.CharField(max_length=255)
+    text = models.TextField()
+    rating = models.IntegerField(default=0)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    category = models.ManyToManyField(Category, through='PostCategory')
+
+    def __str__(self):
+        # return f"[ID={str(self.pk)}, TYPE={self.type}] - header={self.header} (rate:{self.rating})"
+        return self.title
 
     def like(self):
-        self.post_rating += 1
+        self.rating += 1
+        self.save()
 
     def dislike(self):
-        self.post_rating -= 1
+        self.rating -= 1
+        self.save()
 
     def preview(self):
-        return self.post_text[0:100]
+        return self.text[:124]+'...'
 
 
 class PostCategory(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return self.pk
+        # return f'ID={self.pk}:({self.post.pk} -> {self.category.pk})\n'
+
 
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     text = models.TextField(null=True)
-    create_time = models.DateTimeField(null=False)
+    create_time = models.DateTimeField(auto_now_add=True)
     rating = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.text
+        # return f'User({self.user.username}) to post ({self.post.header[:25]}...): "{self.text[:25]}, rate: {self.rating}"'
 
     def like(self):
         self.rating += 1
+        self.save()
 
     def dislike(self):
         self.rating -= 1
+        self.save()
--- Django-projects/NewsPaper/temp.py
+++ Django-projects/NewsPaper/temp.py
@@ -1,29 +1,80 @@
-from django.db import models
+"""
+python manage.py makemigrations
+python manage.py migrate
+python manage.py createsuperuser
+python manage.py shell
+"""
+#Inshell:
+# Подключаем модели
+from django.contrib.auth.models import User
+from news.models import Author, Category, Post, PostCategory, Comment
 
+# Создать двух пользователей (с помощью метода User.objects.create_user). Создадим 4-х пользвателей
+User.objects.create_user(username='Alex')
+User.objects.create_user(username='Boris')
+User.objects.create_user(username='Cate')
+User.objects.create_user(username='Diana')
 
-class Staff(models.Model):
-    full_name = models.CharField(max_length = 255)
-    position = models.CharField(max_length = 255)
-    labor_contract = models.IntegerField()
+# Создать два объекта модели Author, связанные с пользователями.
+Author.objects.create(username=User.objects.get(username = 'Alex'),name = 'Alex')
+Author.objects.create(username=User.objects.get(username = 'Boris'),name = 'Boris')
 
+# Добавить 4 категории в модель Category.
+Category.objects.create(name='Экономика')
+Category.objects.create(name='Спорт')
+Category.objects.create(name='Наука')
+Category.objects.create(name='Политика')
 
-class Products(models.Model):
-    name = models.CharField(max_length = 255)
-    price = models.FloatField(default = 0.0)
-    composition = models.TextField(default = "Состав не указан")
+# Добавить 2 статьи и 1 новость.
+Post.objects.create(type='AR', author=Author.objects.get(name='Alex'), header='Экологичная конверсия', text= 'Российским учёным удалось увеличить эффективность сжигания угля и на 40% снизить выбросы угарного газа при его сгорании. Исследователи установили, что использование определённых солей металлов в качестве катализаторов позволяет в три раза уменьшить объём несгоревшего остатка при сжигании угля и увеличить количество выделяемого полезного тепла. Также специалисты отметили, что процесс горения благодаря применению добавок стал более управляемым.')
+Post.objects.create(type='AR', author=Author.objects.get(name='Boris'), header='Год со дня гибели легенды НБА', text='26 января 2020 года авиакатастрофа унесла жизнь Коби Брайанта. В результате крушения частного вертолёта погибли восемь человек, включая самого баскетболиста, его друзей, а также 13-летнюю дочь Джианну. Дома атлета, которому шёл 42-й год, ждали жена Ванесса и трое детей. О наследии легенды НБА — в материале RT.')
+Post.objects.create(author=Author.objects.get(name='Boris'), header='Денежное первенство: Китай обошёл США', text='В понедельник, 25 января, показатели фондового рынка Китая выросли до максимальных значений с 2015 года. Участники рынка позитивно восприняли информацию о том, что республика обошла США по уровню прямых иностранных инвестиций (ПИИ). По оценкам ООН, в 2020 году зарубежные инвесторы вложили в китайские предприятия $163 млрд, а в американские — $134 млрд.')
 
+# Присвоить им категории (как минимум в одной статье/новости должно быть не меньше 2 категорий).
+# Добавим категорию для статьи ID:1
 
-class Orders(models.Model):
-    time_in = models.DateTimeField(auto_now_add=True)
-    time_out = models.DateTimeField(null=True)
-    cost = models.FloatField(default=0.0)
-    take_away = models.BooleanField(default=False)
-    complete = models.BooleanField(default=False)
-    staff = models.ForeignKey(Staff, on_delete=models.CASCADE)
-    products = models.ManyToManyField(Products, through='ProductOrder')
+# Вариант 1:
+post = Post.objects.get(pk=1)
+category = Category.objects.get(name='Наука')
+post.category.add(category)
 
+# или Вариант 2:
+Post.objects.get(pk=1).category.add(Category.objects.get(name='Политика'))
 
-class ProductOrder(models.Model):
-    product = models.ForeignKey(Products, on_delete=models.CASCADE)
-    in_order = models.ForeignKey(Orders, on_delete=models.CASCADE)
-    amount = models.IntegerField(default=1)
+# Удалим, ибо ошиблись
+PostCategory.objects.filter(pk=1).delete() #Удаление записи с нужным ID
+
+# Снова добавим категорию для статьи ID:1
+Post.objects.get(pk=1).category.add(Category.objects.get(name='Политика'))
+
+# Добавим категорию для статьи ID:2
+Post.objects.get(pk=2).category.add(Category.objects.get(name='Спорт'))
+
+# И добавим категории в новость (ID:3)
+Post.objects.get(pk=3).category.add(Category.objects.get(name='Экономика'))
+Post.objects.get(pk=3).category.add(Category.objects.get(name='Политика'))
+
+# Создать как минимум 4 комментария к разным объектам модели Post (в каждом объекте должен быть как минимум один комментарий).
+Comment.objects.create(post=Post.objects.get(pk=1), user=User.objects.get(username='Cate'), text='Отличная научная статейка, пишите еще.')
+Comment.objects.create(post=Post.objects.get(pk=2), user=User.objects.get(username='Diana'), text='Жалко парня, но жизнь есть жизнь.')
+Comment.objects.create(post=Post.objects.get(pk=3), user=User.objects.get(username='Cate'), text='Китайцы молодцы! Через пару лет порвут США как тузик грелку.')
+Comment.objects.create(post=Post.objects.get(pk=3), user=User.objects.get(username='Alex'), text='Мне, как гражданину США, больно читать комментарии предыдущего оратора.')
+
+# Применяя функции like() и dislike() к статьям/новостям и комментариям, скорректировать рейтинги этих объектов.
+Post.objects.get(pk=1).like()
+Post.objects.get(pk=2).dislike()
+Comment.objects.get(pk=1).like()
+
+# Обновить рейтинги пользователей
+for authors in Author.objects.all():
+    Author.objects.get(name=authors.name).update_rating()
+
+# Вывести username и рейтинг лучшего пользователя (применяя сортировку и возвращая поля первого объекта).
+Author.objects.all().order_by('-rating').values('name', 'rating')
+
+# Вывести дату добавления, username автора, рейтинг, заголовок и превью лучшей статьи, основываясь на лайках/дислайках к этой статье.
+Post.objects.all().order_by('-rating').values('create_date', 'author__name', 'rating','header')[0]
+Post.objects.all().order_by('-rating').preview()
+
+# Вывести все комментарии (дата, пользователь, рейтинг, текст) к этой статье.
+Comment.objects.filter(post=Post.objects.all().order_by('-rating')[0]).values('create_time', 'user__username', 'rating', 'text')
